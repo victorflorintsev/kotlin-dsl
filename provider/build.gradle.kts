394358261fa8e073ebb5a9c15d46e7f5e8eb7204
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2017-11-22 12:03:16 -0200

    Polish `provider/build.gradle.kts`

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -32,36 +32,28 @@ java.sourceSets["main"].kotlin {
     srcDir(apiExtensionsOutputDir)
 }
 
-val generateKotlinDependencyExtensions by task<GenerateKotlinDependencyExtensions> {
-    val publishedPluginsVersion: String by rootProject.extra
-    outputFile = File(apiExtensionsOutputDir, "org/gradle/kotlin/dsl/KotlinDependencyExtensions.kt")
-    embeddedKotlinVersion = kotlinVersion
-    kotlinDslPluginsVersion = publishedPluginsVersion
-    kotlinDslRepository = kotlinRepo
-}
-
-val generateExtensions by tasks.creating {
-    dependsOn(generateKotlinDependencyExtensions)
-}
-
-val compileKotlin by tasks
-compileKotlin.dependsOn(generateExtensions)
-
-val clean: Delete by tasks
-clean.delete(apiExtensionsOutputDir)
-
-
-// -- Testing ----------------------------------------------------------
-val prepareIntegrationTestFixtures by rootProject.tasks
-val customInstallation by rootProject.tasks
 tasks {
+
+    val generateKotlinDependencyExtensions by creating(GenerateKotlinDependencyExtensions::class) {
+        val publishedPluginsVersion: String by rootProject.extra
+        outputFile = File(apiExtensionsOutputDir, "org/gradle/kotlin/dsl/KotlinDependencyExtensions.kt")
+        embeddedKotlinVersion = kotlinVersion
+        kotlinDslPluginsVersion = publishedPluginsVersion
+        kotlinDslRepository = kotlinRepo
+    }
+
+    "compileKotlin" {
+        dependsOn(generateKotlinDependencyExtensions)
+    }
+
+    "clean"(Delete::class) {
+        delete(apiExtensionsOutputDir)
+    }
+
+    val prepareIntegrationTestFixtures by rootProject.tasks
+    val customInstallation by rootProject.tasks
     "test" {
         dependsOn(prepareIntegrationTestFixtures)
         dependsOn(customInstallation)
     }
 }
-
-// --- Utility functions -----------------------------------------------
-inline
-fun <reified T : Task> task(noinline configuration: T.() -> Unit) = tasks.creating(T::class, configuration)
-
